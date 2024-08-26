0245f5c8d3355b0803cc4c745f1e30949153bd43
Tom Belanger <spltt.tlb@gmail.com>
2024-08-26 18:48:28 -0500

    changed to allow flexible calling remote

--- db_access_code.R
+++ db_access_code.R
@@ -5,6 +5,17 @@ library(dbplyr)
 library(tidyverse)
 library(RMySQL)
 
+if (Sys.info()['user'] == "root") {
+  
+  config_path <- "/opt/shiny-server/samples/sample-apps/config/config.yml"
+  
+} else {
+  
+  config_path <- "http://10.10.70.65:3838/sample-apps/config/config.yml"
+  
+}
+
+
 dw <- config::get(value = "datawarehouse", file = config_path)
 
 conn <- dbConnect(RMySQL::MySQL(),
